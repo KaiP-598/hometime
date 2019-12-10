342d5ca8f3ee3e0345c8e33691eedb6cdec4184c
Kai <kaipeng.wu@virginpulse.com>
2019-12-11 09:44:11 +1100

    Deleted API-related codes in ViewController

--- HomeTime/Screens/HomeTime/View/HomeTimeViewController.swift
+++ HomeTime/Screens/HomeTime/View/HomeTimeViewController.swift
@@ -77,90 +77,10 @@ extension HomeTimeViewController {
   func loadTramData() {
     loadingNorth = true
     loadingSouth = true
+    viewModel?.loadNorthTrams.onNext(())
+    viewModel?.loadSouthTrams.onNext(())
 
-//    if let token = token {
-//      print("Token \(token)")
-//      loadTramDataUsing(token: token)
-//    } else {
-//      fetchApiToken { [weak self] token, error in
-//        if let token = token, error == nil  {
-//          self?.token = token
-//          print("Token: : \(String(describing: token))")
-//          self?.loadTramDataUsing(token: token)
-//        } else {
-//          self?.loadingNorth = false
-//          self?.loadingSouth = false
-//          print("Error retrieving token: \(String(describing: error))")
-//        }
-//      }
-//    }
-  }
-
-  func fetchApiToken(completion: @escaping (_ token: String?, _ error: Error?) -> Void) {
-    let tokenUrl = "http://ws3.tramtracker.com.au/TramTracker/RestService/GetDeviceToken/?aid=TTIOSJSON&devInfo=HomeTimeiOS"
-
-    loadTramApiResponseFrom(url: tokenUrl) { response, error in
-      let tokenObject = response?.first
-      let token = tokenObject?["DeviceToken"] as? String
-      completion(token, error)
     }
-  }
-
-  func loadTramDataUsing(token: String) {
-    let northStopId = "4055"
-    let northTramsUrl = urlFor(stopId: northStopId, token: token)
-    loadTramApiResponseFrom(url: northTramsUrl) { [weak self] trams, error in
-      self?.loadingNorth = false
-
-      if error != nil {
-        print("Error retrieving trams: \(String(describing: error))")
-      } else {
-        self?.northTrams = trams
-        self?.tramTimesTable.reloadData()
-      }
-    }
-
-    let southStopId = "4155"
-    let southTramsUrl = urlFor(stopId: southStopId, token:token)
-    loadTramApiResponseFrom(url: southTramsUrl) { [weak self] trams, error in
-      self?.loadingSouth = false
-
-      if error != nil {
-        print("Error retrieving trams: \(String(describing: error))")
-      } else {
-        self?.southTrams = trams;
-        self?.tramTimesTable.reloadData()
-      }
-    }
-
-  }
-
-  func urlFor(stopId: String, token: String) -> String {
-    let urlTemplate = "http://ws3.tramtracker.com.au/TramTracker/RestService/GetNextPredictedRoutesCollection/{STOP_ID}/78/false/?aid=TTIOSJSON&cid=2&tkn={TOKEN}"
-    return urlTemplate.replacingOccurrences(of: "{STOP_ID}", with: stopId).replacingOccurrences(of: "{TOKEN}", with: token)
-  }
-
-  func loadTramApiResponseFrom(url: String, completion: @escaping (_ responseData: [JSONDictionary]?, _ error: Error?) -> Void) {
-    let task = session?.dataTask(with: URL(string: url)!) { data, response, error in
-      if error != nil {
-        completion(nil, error)
-      } else {
-        do {
-          if let data = data,
-            let jsonResponse = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? JSONDictionary {
-            let objects = jsonResponse["responseObject"] as? [JSONDictionary]
-            completion(objects, nil)
-          } else {
-            completion(nil, JSONError.serialization)
-          }
-        } catch {
-          completion(nil, JSONError.serialization)
-        }
-      }
-    }
-
-    task?.resume()
-  }
 
   func tramsFor(section: Int) -> [Tram]? {
     if section == 0 {
